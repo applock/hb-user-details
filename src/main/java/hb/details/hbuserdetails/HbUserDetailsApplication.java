579224a72b5a67e1896cbd27b6277267692153b2
Iman <ibiswas@adobe.com>
2021-07-05 16:23:26 +0530

    Initial checkin

---
+++ src/main/java/hb/details/hbuserdetails/HbUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package hb.details.hbuserdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbUserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbUserDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hb/details/hbuserdetails/HbUserDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package hb.details.hbuserdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbUserDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
